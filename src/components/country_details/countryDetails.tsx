352008dae576b0d9e3c51780891d88ad39483afb
Andy Liu <aliu927@gmail.com>
2022-09-30 22:19:38 -0700

    figured out useParams

--- src/components/country_details/countryDetails.tsx
+++ src/components/country_details/countryDetails.tsx
@@ -1,14 +1,9 @@
-import { Outlet } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 export default function CountryDetails() {
   //const countryClicked = useParams(); => This will return obj w/ KeyPair:Values
   //use Fetch API => Take value from countryCLick Obj
   //Render country items based off the Fetch API
+  const { nameOfCountry } = useParams();
 
-  return (
-    <div>
-      Hello
-      <Outlet />
-    </div>
-  );
+  return <div>Country:{nameOfCountry}</div>;
 }
