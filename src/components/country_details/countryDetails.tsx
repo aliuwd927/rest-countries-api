b05d92cef1ad7f357796ac49dbc5d64c02245865
Andy Liu <aliu927@gmail.com>
2022-10-06 00:23:44 -0700

    setting divs

--- src/components/country_details/countryDetails.tsx
+++ src/components/country_details/countryDetails.tsx
@@ -39,46 +39,53 @@ export default function CountryDetails(props: CountryDetailsProps) {
             <Link to={"/"}>
               <button>Back</button>
             </Link>
-            <br />
-            <img src={element.flags.png} alt="" />
-            <br />
-            <div>NavtiveName:{element.name.official}</div>
-            <br />
-            <div>Population:{element.population}</div>
-            <br />
-            <div>Region:{element.region}</div>
-            <br />
-            <div>Sub-Region:{element.subregion}</div>
-            <br />
-            <div>Capital:{element.capital}</div>
-            <br />
-            <div>Top Level Domain:{element.tld}</div>
-            <br />
-            <div>
-              Currency:
-              {Object.values(countryCurrency).map((currency) => {
-                return <div>{currency.name}</div>;
-              })}
+            <div className="country_Detail_Flag">
+              <img src={element.flags.png} alt="" />
             </div>
-            <br />
-            <div>
-              Language:
-              {Object.values(countryLanuage).map((language) => {
-                return <div>{language}</div>;
-              })}
-            </div>
-            <br />
-            <div>
-              Border:
-              {element.borders?.map((border) => {
-                return (
-                  <div>
-                    <Link to={`/country_borders/${border}`}>
-                      <button>{border}</button>
-                    </Link>
-                  </div>
-                );
-              })}
+
+            <div className="country_Detail_Stats">
+              <div className="country_Details_Stats_L">
+                <div>NavtiveName:{element.name.official}</div>
+
+                <div>Population:{element.population}</div>
+
+                <div>Region:{element.region}</div>
+
+                <div>Sub-Region:{element.subregion}</div>
+
+                <div>Capital:{element.capital}</div>
+              </div>
+
+              <div className="country_Details_Stats_R">
+                <div>Top Level Domain:{element.tld}</div>
+
+                <div>
+                  Currency:
+                  {Object.values(countryCurrency).map((currency) => {
+                    return <div>{currency.name}</div>;
+                  })}
+                </div>
+
+                <div>
+                  Language:
+                  {Object.values(countryLanuage).map((language) => {
+                    return <div>{language}</div>;
+                  })}
+                </div>
+              </div>
+
+              <div>
+                Border:
+                {element.borders?.map((border) => {
+                  return (
+                    <div>
+                      <Link to={`/country_borders/${border}`}>
+                        <button>{border}</button>
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         );
